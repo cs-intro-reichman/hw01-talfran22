6cb6bdb12786e9ed6d62219457d837b20a9d82ce
talfran22 <154238085+talfran22@users.noreply.github.com>
2023-12-18 20:56:48 +0200

    LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -6,4 +6,11 @@
  */
 public class LinearEq {
 	// Put your code here
+	public static void main(String[] args) {
+		double a = Double.parseDouble(args[0]);
+		double b = Double.parseDouble(args[1]);
+		double c = Double.parseDouble(args[2]);
+		System.out.println(a + " * x + " + b + " = " + c);
+		System.out.println("x = " + ((c - b)/a));
+	}
 }
