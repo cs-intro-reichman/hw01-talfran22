3b420fb9b39280ad411e128ef06f1b39137e68fd
talfran22 <154238085+talfran22@users.noreply.github.com>
2023-12-18 20:58:30 +0200

    Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -6,5 +6,11 @@
 public class Triangle {
 	public static void main(String[] args) {
 		// Put your code here	
+		int side1 = Integer.parseInt(args[0]);
+		int side2 = Integer.parseInt(args[1]);
+		int side3 = Integer.parseInt(args[2]);
+		boolean triangle;
+		triangle = ((side1+side2) > side3 && (side1+side3) > side2 && (side2 + side3) > side1);
+		System.out.println(side1 + ", " + ", " + side2 + ", " + side3 + ": " + triangle);
 	}
 }
