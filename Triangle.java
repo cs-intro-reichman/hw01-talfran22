06a392bd598617e84e2190c1599aaf0137867e7a
runiAviv <155253539+runiAviv@users.noreply.github.com>
2024-01-02 20:53:50 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -9,8 +9,8 @@ public class Triangle {
 		int side1 = Integer.parseInt(args[0]);
 		int side2 = Integer.parseInt(args[1]);
 		int side3 = Integer.parseInt(args[2]);
-		boolean triangle;
-		triangle = ((side1+side2) > side3 && (side1+side3) > side2 && (side2 + side3) > side1);
+		// #feedback: you can set value within same line of variable declaration
+		boolean triangle = ((side1+side2) > side3 && (side1+side3) > side2 && (side2 + side3) > side1);
 		System.out.println(side1 + ", "  + side2 + ", " + side3 + ": " + triangle);
 	}
 }
