56d3676996f490a4d1c1e9807792b8ad1bfd1454
runiAviv <155253539+runiAviv@users.noreply.github.com>
2024-01-02 20:46:06 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -2,6 +2,7 @@ public class AddTwo {
 	public static void main(String[] args) {
 		int x = Integer.parseInt(args[0]);
 		int y = Integer.parseInt(args[1]);
+		// #Feedback: better to not do calculation in print commands
 		System.out.println(x + " + " + y + " = " + (x + y));
 	}
 }
